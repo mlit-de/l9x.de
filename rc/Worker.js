599af1aedf24253391bc7640da8f8b95e9f14a55
Markus Lauer <markus.lauer-2015@milt.de>
2022-12-29 19:54:25 +0100

    bugfix

--- rc/Worker.js
+++ rc/Worker.js
@@ -16,9 +16,18 @@ onconnect = function (event) {
             portBySec[e.data.sec]=e.ports[0]
             portByFp[e.data.fp]=e.ports[0]
             fp.push(e.data.fp)
+            //nr=ports.length
+            //ports.push(port)
+            //port.postMessage({type: "registered", id: nr})
         }
         if(e.data.t=="show") {
             port.postMessage({t:"registered", fp: fp, sec: Object.keys(portBySec)})
+        }
+        if(e.data.t=="clear") {
+            portByFp={}
+            portBySec={}
+            fp=[]
+            port.postMessage({t:"registered", fp: Object.keys(portByFp), sec: Object.keys(portBySec)})
         }
         if(e.data.t=="connect") {
             var p=portByFp[e.data.fp]
@@ -31,5 +40,19 @@ onconnect = function (event) {
                 e.ports[0].close()
             }
         }
+        if(e.data.t=="send-debug") {
+            var p=portByFp[e.data.trg]
+            if(p!=void 0) {
+                console.log("Worker:Send-debug")
+                p.postMessage(e.data.msg)
+            } else {
+                console.log("Worker:Unknown-Target")
+            }
+        }
+        /*
+        if(e.data.type=="post") {
+            ports[e.data.dest].postMessage({type:"post", msg: e.data.msg},e.ports)
+        }
+         */
     };
 };
